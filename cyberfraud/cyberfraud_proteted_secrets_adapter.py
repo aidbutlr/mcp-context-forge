bac17a49b11bf5e84d435ae76c762512045fda73
Aidan Butler <aidbutlr@ie.ibm.com>
2026-03-02 21:28:45 +0000

    CYFR-209 protected secrets adapter

--- cyberfraud/cyberfraud_proteted_secrets_adapter.py
+++ cyberfraud/cyberfraud_proteted_secrets_adapter.py
@@ -56,36 +56,6 @@ def read_protected_secrets() -> None:
         os.environ["REDIS_URL"] = os.path.expandvars(os.environ["REDIS_URL"])
         print(f"REDIS_URL: {os.environ['REDIS_URL']}")
 
-        
-read_protected_secrets()
 
-"""
-
-
-project_root = Path(__file__).resolve().parents[1]  # adjust as needed
-sys.path.insert(0, str(project_root)+"/cyberfraud")
-# Third-Party
-
-_real_getenv = os.environ.get
-
-
-def custom_getenv(key, default=None):    
-    if key in protected_secrets_map:
-        print(f">>>>Getting Protected Secret {key}")
-        ps_path = protected_secrets_map[key]
-        nodes = ps_path.split("__")
-        loc =protected_secrets_dict
-        for node in nodes:
-            if node in loc:
-                loc = loc[node]
-            else:
-                print(f"Entry not found in Protected Secrets {node}")
-                break
-        return loc
-        return protected_secrets_dict
-    else:
-        #print(f">>>>Getting Environment Secret {key}")
-        return _real_getenv(key, default)
-
-os.environ.get = custom_getenv
-"""
+if "USE_PROTECTED_SECRETS" in os.environ and os.environ["USE_PROTECTED_SECRETS"] == "True" :
+    read_protected_secrets()
