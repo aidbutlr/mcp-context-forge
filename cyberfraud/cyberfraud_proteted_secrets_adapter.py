9963edaf4b63bd9a40fba787fce608c238abecb1
Aidan Butler <aidbutlr@ie.ibm.com>
2026-03-02 19:11:40 +0000

    CYFR-209 protected secrets adapter

--- cyberfraud/cyberfraud_proteted_secrets_adapter.py
+++ cyberfraud/cyberfraud_proteted_secrets_adapter.py
@@ -49,10 +49,10 @@ def read_protected_secrets() -> None:
                 break
         os.environ[key]=str(value)
         
-    if os.environ["DATABASE_URL"]:
+    if "DATABASE_URL" in os.environ:
         os.environ["DATABASE_URL"] = os.path.expandvars(os.environ["DATABASE_URL"])
         print(f"DATABASE_URL: {os.environ['DATABASE_URL']}")
-    if os.environ["REDIS_URL"]:
+    if "REDIS_URL" in os.environ:
         os.environ["REDIS_URL"] = os.path.expandvars(os.environ["REDIS_URL"])
         print(f"REDIS_URL: {os.environ['REDIS_URL']}")
 
