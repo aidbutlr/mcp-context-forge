9a3619689dcc4b468c3ddd10b6696fcd1471f81a
Aidan Butler <aidbutlr@ie.ibm.com>
2026-03-04 18:10:49 +0000

    CYFR-209 protected secrets adapter

--- .sps/custom_compliance_checks.sh
+++ .sps/custom_compliance_checks.sh
@@ -4,6 +4,7 @@ export PATH=/root/.local/bin:$PATH
 source $WORKSPACE/$PIPELINE_CONFIG_REPO_PATH/scripts/utilities/python_utils.sh
 source $WORKSPACE/$PIPELINE_CONFIG_REPO_PATH/scripts/utilities/go_utils.sh
 install_python3 3.11
+install_go
 # Go causing mend failure
 rm -rf  mcp-servers/go
 pip3.11 install --upgrade pip 
